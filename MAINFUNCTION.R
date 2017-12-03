23fa88056de0010424872d7fb706d19e96c5c38c
Juan Camilo Rivera P <lacicladelosdos@gmail.com>
2017-12-03 17:18:12 -0500

    JCR

--- MAINFUNCTION.R
+++ MAINFUNCTION.R
@@ -1,25 +1,66 @@
 #Load functions
 source("HOURLYCONTROL.R")
+source("CONVERTHOURLYTODAILY.R")
 
 #Set working directory where are weathers data.
 setwd("D:/OneDrive - CGIAR/GitHub/Nuevo Codigo Clima/Datos_Sonora/Datos")
+setwd("C:/Users/JCRIVERA/Documents/Codigo_Nuevo_Clima/Datos/Datos_Prueba")
 
 #Restrictions as data frame
 Variables <- c("Vmin", "Vmax")
-TX <- c(40,0)
-TM <- c(40,0)
+TX <- c(41,0)
+TM <- c(41,0)
 SR <- c(1600,0)
+RH <- c(100,0)
 LONG <- c(-110.119, NA)
 LAT <- c(27.51458, NA)
 TZ <- c("Etc/GMT+7", NA)
+#Start_date <- c("2013-1-1")
+#End_date <- c("2017-12-31")
 
-Hourly_restric <- data.frame(TX, TM, SR, LONG, LAT, TZ)
+Start_date <- c("2013-1-1")
+End_date <- c("2017-12-31")
+
+Hourly_restric <- data.frame(TX, TM, SR,RH, LONG, LAT, TZ)
+
+final_results <- mclapply(list.files(), results, restricfile = Hourly_restric ,mc.cores=20)
+final_results <- do.call("rbind", final_results)
+colnames(final_results) <- c("Station_Name", "Variable_Name", "OriginalData_Size", "CleanData_Size", "ErrorData_Size")
+write.csv(final_results, file = "Results_HourlyControl.csv")
 
 
+#Path for hourly files. Copy and paste files to Hourly_Data
+dir.create(file.path(getwd(), "Hourly_Data"), showWarnings = FALSE)
 
 
+#Path for hourly files. Copy and paste files to Hourly_Data
+path_hourly <- paste(getwd(), "Hourly_Data", sep = "/")
+move_files_txt(from = getwd(), to = path_hourly)
+
+#Change Directory
+setwd(path_hourly)
+
+#Hourly to Daily
+#The percentage is used for checking if a day has enough data.   
+mclapply (list.files(pattern = "\\.txt$"), Hour_to_Day, percentage = 0.8,mc.cores=20)
+
+#Change Directory
+path_daily <- paste(getwd(), "Daily_Data", sep = "/")
+setwd(path_daily)
+
+#Path for files with Rmwagen format. Copy and paste files to Rmwagen
+dir.create(file.path(getwd(), "Rmawgen"), showWarnings = FALSE)
+
+#Variables para Rmwagen
+put_rmawgenformat(list.files(pattern = "\\.txt$"), 'TX', Start_date, End_date)
+put_rmawgenformat(list.files(pattern = "\\.txt$"), 'TM', Start_date, End_date)
+put_rmawgenformat(list.files(pattern = "\\.txt$"), 'P', Start_date, End_date)
 
 
-results <- lapply(list.files(), function (x, restricfile = Hourly_restric) results(x))
-results <- lapply(list.files(), results, restricfile = Hourly_restric)
+#move files to Rmawgen folder
+Rmawgen <- paste(getwd(), "Rmawgen", sep = "/")
+move_files_txt(from = getwd(), to = Rmawgen, format = "\\.csv$")
+
+
+setwd(Rmawgen)
 
