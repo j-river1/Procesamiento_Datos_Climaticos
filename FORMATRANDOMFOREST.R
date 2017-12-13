de518632da6eccf3f89d01fa9700bb60275b3f62
Juan Camilo Rivera P <lacicladelosdos@gmail.com>
2017-12-13 09:43:06 -0500

    JCR

--- FORMATRANDOMFOREST.R
+++ FORMATRANDOMFOREST.R
@@ -3,21 +3,13 @@
 #match_files merge the files according their names
 #Arguments      -listFiles list of files
 
-subDir <- "Random_Forest"
-subDir <- "File_Variables"
-
-dir.create(file.path(getwd(), subDir), showWarnings = FALSE)
 
 match_files  <- function(listFiles, resumefile)
 {
-
-  
+ 
   filenames <- read.csv(resumefile, header=T)
   names_stations <- unique(filenames$Station_Name)
-  
-  
-  
-  
+    
   #Grouping according names station
   files  <- lapply(names_stations, merge_files, listfiles = list.files())
 
@@ -34,7 +26,8 @@ merge_files <- function (name, listfiles)
   tablaPerday <- Reduce(function(dtf1, dtf2) merge(dtf1, dtf2, by = "Date", all = TRUE),read_files)
   
   namec <- paste0(name,".txt" )
-  write.table(tablaPerday, file = paste(getwd(),"Random_Forest", namec, sep =  "/"), row.names = FALSE, quote = FALSE, col.names = TRUE)
+  #write.table(tablaPerday, file = paste(getwd(),"Random_Forest", namec, sep =  "/"), row.names = FALSE, quote = FALSE, col.names = TRUE)
+  write.table(tablaPerday, file = paste("../../RandomForest", namec, sep =  "/"), row.names = FALSE, quote = FALSE, col.names = TRUE)
   return(tablaPerday)
 }
 
@@ -51,28 +44,45 @@ label_for_files <- function (name)
 
 #Read
 
-leer_label <- function(name)
-{
-  file <- read.table(name, header=T)
-  variable <-  split_name(name)[2]
-  names(file) <- c("Date", variable)
+#leer_label <- function(name)
+#{
+#  file <- read.table(name, header=T)
+#  variable <-  split_name(name)[2]
+#  names(file) <- c("Date", variable)
   
-  return(file) 
+#  return(file) 
   
-}
+#}
   
 #move files SR and HR from Daily Hour to Files_by_station
 
 move_files_SR_HR <- function ()
 {
-    list.files("../../AfterDailyControl_Data", pattern ="_SR_")
-    paste0("")
-    list.files("../../AfterDailyControl_Data", pattern ="_RH_")
+    list_files_SR <- list.files("../../AfterDailyControl_Data", pattern ="_SR_")
+    list_files_RH <- list.files("../../AfterDailyControl_Data", pattern ="_RH_")
     
+    lapply(list_files_SR, copy_paste_files)
+    lapply(list_files_RH, copy_paste_files)
     
 }
 
-paste_name <_ function()
+#paste_name works for delete the last part of name. e.g. Chihauaita_SR_WAM2 converts to Chihauaita_SR
+paste_name <- function(namefile)
+{
+    split_na <-  split_name(namefile) [3] 
+    name_comple <- gsub(paste0("_",split_na),"",namefile)
+    return (name_comple)
+}
+
+#copy_extension works for making path of files
+copy_paste_files <- function(namefile)
+{
+    name_comp <- paste_name(namefile)
+    path_from <- paste0("../../AfterDailyControl_Data/", namefile)
+    path_to <- paste0("../../Rmawgen/Files_By_Station/", name_comp)
+    file.copy(path_from, path_to)
+
+}
 
 
 
