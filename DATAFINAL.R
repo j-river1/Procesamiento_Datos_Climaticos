f6bb5c46c56856aaf294516c2cd0bb56bdb4c89f
Juan Camilo Rivera P <lacicladelosdos@gmail.com>
2017-12-18 15:36:45 -0500

    jcr

--- DATAFINAL.R
+++ DATAFINAL.R
@@ -2,89 +2,22 @@
 #Arguments listfiles is a list of files with pattern txt.
 read_files <- function (listfiles)    
 {
-    
-    
-    
+       
     files_merge <- lapply(listfiles,merge_tables)
     names_filemerge <- lapply(listfiles, function(x) {names = split_name(x) 
                                                          return (names)})
     names(files_merge) <- names_filemerge
     variable_names <- names(files_merge)
-
-#     save_files <- lapply(files_merge, function(x) { name <- names(files_merge)
-#                                                     
-#                                                     write.table(files_merge, file = paste0("./Original_Data/", name, ".txt" ))})
         
     save_files <- lapply(seq_along(files_merge), function(y,n,i){
                                                                 name <- n[[i]]
                                                                 write.table(y[[i]], file = paste0("../Final_Data/", name, ".txt" ))
                                                                 }, y=files_merge, n=names(files_merge))
-    
-#     #Read files with missing values
-#     names_Incomple <- lapply(listfiles, function(x) {names = split_name(x) 
-#                                                      return (names)})
-#     files_Incomple <- lapply(listfiles, function(x)  { file = read.table(x, header= TRUE)  
-#                                                        return (file)})
-#     
-#     names(files_Incomple) <- names_Incomple
-#     files_Incomple <- files_Incomple[order(names(files_Incomple))]
-#     
-#     
-#     
-#     
-#     
-#     
-#     #Read files with SR value
-#     names_SRfiles <- lapply(list.files(path= "./SR"), function(x) {names = split_name(x)[1]
-#                                                                    return (names)})
-#     files_SR <-lapply(list.files(path= "./SR"), function(x)  { file = read.table(paste0("./SR/",x), header= TRUE)
-#                                                                names(file) = c("Date", "SR" ) 
-#                                                                return (file) })
-#     names(files_SR) <- names_SRfiles
-#     
-#     files_SR <- files_SR[order(names(files_SR))]
-#     
-#     
-#     
-#     
-#     #Read files with RH value
-#     names_RHfiles <- lapply(list.files(path= "./RH"), function(x) {names = split_name(x)[1]
-#                                                                    return (names)})
-#             
-# #     files_RH <-lapply(list.files(path= "./RH"), function(x) {file = read.table(paste0("./RH/",x), header= TRUE)    
-# #                                                                  names(file) = c("Date", "RH" )
-# #                                                                                    return (file)
-# #                                                                                               })
-#     
-#      files_RH <-lapply(list.files(path= "./RH"), function(x) {file = read.table(paste0("./RH/",x), header= TRUE)    
-#                                                                   names(file) = c("Date", "RH" )
-#                                                                                     return (file)
-#                                                                                                })
-# 
-# 
-# 
-# 
-#     names(files_RH) <- names_RHfiles
-#     
-#     files_RH <- files_RH[order(names(files_RH))]
-#     
-#     
-#     
-#     #merge tables Incomple with SR
-# 
-#     
-#      w <- mapply(merge_tables,files_Incomple, files_SR = files_SR)
-    return ( files_merge)
-
-
-
-
-    
+ 
+    return ( files_merge)    
 }
 
 
-#merge_tables 
-#listfiles with partern
 
 merge_tables <- function(namefile)
 {
